50c5d0c99a93cb7588ad909dbc92d18eda7270a4
Лада Реботунова <redpen11097@yandex.ru>
2025-10-21 21:44:36 +0300

    a0-a2 homework

---
+++ a2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("     *\n");
+    printf("    ***\n");
+    printf("   *****\n");
+    printf("  *******\n");
+    printf("   HHOHH\n");
+    printf("   ZZZZZ\n");
+    return 0;
+}
